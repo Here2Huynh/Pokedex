df6b8687953b2ab8d3b520d06fef9bcc94c550b4
John Huynh <jhnhnyh9@gmail.com>
2021-12-14 19:22:07 -0800

    chore(setup): set up project. add basic nav and layout.

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>Pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>play app using SkelteKit and pokemon api to build a pokedex</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Nav from '../components/nav.svelte';
+	console.log('hi');
+</script>
+
+<svelte:head>
+	<title>Pokedex</title>
+</svelte:head>
+
+<h1>Pokedex</h1>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
